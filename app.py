f6b0c766f314f6b269f68f1d78c6300c08ff64ba
oliverlind <oliverlind1902@gmail.com>
2023-10-16 10:42:05 +0100

    Adding first plots

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ def main_ui():
     if option == 'Intro':
         st.subheader('Introduction')
         st.write("Hi there!")
-        st.write("Welcome to my webpage dedicated to my project on Rydberg atom array quantum computers and simulators.")
+        st.write(
+            "Welcome to my webpage dedicated to my project on Rydberg atom array quantum computers and simulators.")
 
         st.write("Feel free to explore different aspects of my project using the sidebar. I will regularly update "
                  "these sections as I make progress, with further subsections for each aspect.")
@@ -43,28 +44,27 @@ def main_ui():
     if option == 'Code':
         st.subheader("Code set up")
 
-        st.write(" The code for 1D atom arrays as follows:")
-
-        st.write(f"The 'RydbergHamiltonian1D' class computes the 2^n dimensional Hamiltonian matrix "
-                 f"for the following Hamiltonian:")
-
-        st.latex("H = -Σ_i \Delta_i|r_i⟩⟨r_i| + Σ_i,j J_ij * |r_i⟩⟨r_j|")
-
-        st.write("The 'AdiabaticEvolution' class is responsible for evolving the Hamiltonian. This is done via "
-                 "Trotterization for a selected time interval 'dt' of a desired time.")
-
-        st.write("The Hamiltonian can be evolved under different detuning schedules. So far, the two options are:")
-
-        st.write("• Linear Detuning: Pick a start and a stop detuning value and evolve through these values in 't/dt' "
-                 "integer steps.")
-
-        st.write("• Zero Detuning: Hamiltonian is evolved with zero detuning.")
+        # st.write(" The code for 1D atom arrays as follows:")
+        #
+        # st.write(f"The 'RydbergHamiltonian1D' class computes the 2^n dimensional Hamiltonian matrix "
+        #          f"for the following Hamiltonian:")
+        #
+        # #st.latex("H = -Σ_i \Delta_i|r_i⟩⟨r_i| + Σ_i,j J_ij * |r_i⟩⟨r_j|")
+        #
+        # st.write("The 'AdiabaticEvolution' class is responsible for evolving the Hamiltonian. This is done via "
+        #          "Trotterization for a selected time interval 'dt' of a desired time.")
+        #
+        # st.write("The Hamiltonian can be evolved under different detuning schedules. So far, the two options are:")
+        #
+        # st.write("• Linear Detuning: Pick a start and a stop detuning value and evolve through these values in 't/dt' "
+        #          "integer steps.")
+        #
+        # st.write("• Zero Detuning: Hamiltonian is evolved with zero detuning.")
 
     if option == 'Plots':
         types = st.selectbox('Select Type', ['Constant Zero Detunning'])
 
         if types == 'Constant Zero Detunning':
-
             st.write("Below you will find plots illustrating the behaviour of 1D atom arrays being driven by an "
                      "oscillatory field at the Rabi frequency (Energy difference between ground and Rydberg state)")
 
@@ -74,18 +74,33 @@ def main_ui():
                      "expected, each individual atoms Rydberg Fidelity (probability of being in the Rydberg state) "
                      "oscillates at the Rabi frequency. ")
 
-            image = Image.open('Plots/Rabi Oscillations/n=3/Rabi_Osc_No_Int.png')
-            st.image(image)
+            centre_image('Plots/Rabi Oscillations/n=3/Rabi_Osc_No_Int.png')
 
             st.write("Next, we turn on the interaction such that the second atom lies within the Blockade radius of "
                      "the first and third atom. Note: ‘a’ here is the uniform separation of the atoms an ‘R_b’ is the "
                      "blockade radius.")
 
-            image = Image.open('Plots/Rabi Oscillations/n=3/Rabi_Osc_NN.png')
-            st.image(image)
+            centre_image('Plots/Rabi Oscillations/n=3/Rabi_Osc_NN.png')
 
-            image = Image.open('Plots/Rabi Oscillations/n=3/Rabi_Osc_NN_line.png')
-            st.image(image)
+            centre_image('Plots/Rabi Oscillations/n=3/Rabi_Osc_NN_line.png')
+
+            st.write("Now if we increase the time we evolve the system to 12 μs we get interesting plot:")
+
+            centre_image('Plots/Rabi Oscillations/n=3/Rabi_Osc_NN_line_t=12.png')
+
+
+def centre_image(path):
+    col1, col2, col3 = st.columns([1, 6, 1])
+
+    with col1:
+        st.write("")
+
+    with col2:
+        image = Image.open(path)
+        st.image(image)
+
+    with col3:
+        st.write("")
 
 
 if __name__ == "__main__":
